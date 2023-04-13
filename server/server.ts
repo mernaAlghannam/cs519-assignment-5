2b4e92f55a2fa6b20bcb2da69b15b9517a0864a8
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-04-13 09:23:03 -0400

    fixed workflow issues

--- server/server.ts
+++ server/server.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static("../client/build"));
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  // console.log(`Server listening on ${PORT}`);
 });
 
 app.get("/api/hello", (req, res) => {
