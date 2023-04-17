276f591c078557e8c4f207a08c7850505b03bef1
mernaAlghannam <46355198+mernaAlghannam@users.noreply.github.com>
2023-04-17 17:34:11 -0400

    hope it works

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,41 +1,88 @@
 import './App.css';
 import { useState } from 'react';
+import {BlobServiceClient} from "@azure/storage-blob"
 
 function App() {
   let [currShipperID, setCurrShipperID] = useState<string>("");
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [file, setFile] = useState<File >()
 
-  const fetchClassList = async () => {
 
-    // const res = await fetch("https://my-container-apps.bluerock-b7a8c33e.westus2.azurecontainerapps.io/api/HttpExample?name=https://learn.microsoft.com/azure/cognitive-services/computer-vision/media/quickstarts/presentation.png", {
-    //   method: "POST",
-    // }
-    // );
-    const res = await fetch("/api/hello");
+    // // set the current class id to the one selected in dropdown menu
+    // const handleChange = (event: any) => {
+    //   setCurrShipperID(event.target.value);
+    // };
 
-    const shippingData = await res.json();
-    console.log(shippingData)
-
-    // setShippersDataList(shippingData);
-  };
-
-    // set the current class id to the one selected in dropdown menu
-    const handleChange = (event: any) => {
-      setCurrShipperID(event.target.value);
+    const handleChange2 = ( event:any) => {
+      setFile(event.target.files[0])
+      console.log(event.target.files[0].type)
+      console.log(event.target.files[0].name)
     };
   
-    const handleSubmit = (event: any) => {
-      if (currShipperID !== "")
-      fetchClassList();
-      event.preventDefault();
-    };
+    // const handleSubmit = (event: any) => {
+    //   if (currShipperID !== "")
+    //   fetchClassList();
+    //   event.preventDefault();
+    // };
+
+    const handleSubmit2 = async () => {
+      if (!file){
+        return
+      }
+
+      const connectionstring = "BlobEndpoint=https://outqueue.blob.core.windows.net/;QueueEndpoint=https://outqueue.queue.core.windows.net/;FileEndpoint=https://outqueue.file.core.windows.net/;TableEndpoint=https://outqueue.table.core.windows.net/;SharedAccessSignature=sv=2021-12-02&ss=b&srt=co&sp=rwdlaciytfx&se=2023-07-27T08:37:57Z&st=2023-04-14T00:37:57Z&spr=https&sig=aLGp1BRUodkqvHis%2BaFZN2syq3UTESoifbzSPApvUF0%3D"
+      const blobServiceClient = BlobServiceClient.fromConnectionString(connectionstring);
+      const containerName = "image"
+      const containerClient = blobServiceClient.getContainerClient(containerName)
+      console.log("hello")
+      console.log(file.name)
+      const blob = new Blob([file], {type: file.type});
+      const blockBlobClient = containerClient.getBlockBlobClient(file.name)
+      await blockBlobClient.uploadData(blob)
+      alert("successfully added to queue")
+
+      // const res = await fetch("/api/productImage/https://outqueue.blob.core.windows.net/image/commImg");
+      const res = await fetch("https://my-container-apps.bluerock-b7a8c33e.westus2.azurecontainerapps.io/api/HttpExample?name=https://outqueue.blob.core.windows.net/image/commImg.png"
+      );
+
+      const shippingData = await res.json();
+      console.log(shippingData)
+      // await fetchShipperData()
+
+      
+      // const res = await fetch("/api/productImage/https://outqueue.blob.core.windows.net/image/"+file.name);
+
+      // const shippingData = await res.json();
+      // console.log(shippingData)
+
+      alert("successfully added to queue")
+  
+    }
+
+    const fetchShipperData = async () => {
+      if (!file){
+        return
+      }
+
+      const res = await fetch("/api/productImage/https://outqueue.blob.core.windows.net/image/commImg");
+
+      const shippingData = await res.json();
+      console.log(shippingData)
+    }
+
   return (
     <div className="App">
-          <div style={{ width: "100%" }}>
-      <form onSubmit={handleSubmit}>
+          {/* <div style={{ width: "100%" }}> */}
+      {/* <form onSubmit={handleSubmit}>
           <input type="text" value={currShipperID} onChange={handleChange} />
         <input type="submit" value="Submit" />
-      </form>
-          </div>
+      </form> */}
+          {/* </div> */}
+        <form onSubmit={handleSubmit2}>
+          <h1>React File Upload</h1>
+          <input type="file" onChange={handleChange2}/>
+          <button type="submit">Upload</button>
+        </form>
     </div>
   );
 }
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import fetch from "node-fetch"
 
 const PORT = process.env.PORT || 5000;
 
@@ -14,8 +15,36 @@ app.listen(PORT, () => {
 });
 
 app.get("/api/hello", (req, res) => {
+  // const response = fetch()
   res.send({ message: "Hello" });
 });
+
+// app.get('/api/productImage/', async (req, res) => {
+//   // fetch("https://my-container-apps.bluerock-b7a8c33e.westus2.azurecontainerapps.io/api/HttpExample?name="+req.params.imageUrl,
+//   // { method: 'GET'}).then(res => res.json())
+//   // .then(json => res.send(json));;
+
+//   // try {
+//   //   const apiResponse = await fetch(
+//   //     "https://my-container-apps.bluerock-b7a8c33e.westus2.azurecontainerapps.io/api/HttpExample?name=https://outqueue.blob.core.windows.net/image/"+req.params.imageUrl)
+//   //   const apiResponseJson = await apiResponse.json()
+
+//   //   console.log(apiResponseJson)
+//   //   res.send('Running 🏃')
+//   // } catch (err) {
+//   //   console.log(err)
+//   //   res.status(500).send('Something went wrong')
+//   // }
+//   // request.headers({"x-functions-key": process.env.HOSTKEY });
+
+//   // request.end(function (response) {
+//   //   if (response.error) throw new Error(response.error);
+
+//     res.send({ message: "hi" });
+//   //   res.json(response.body || {});
+//   // });
+
+// });
 
 // All other unmatched requests will return the React app
 app.get("/", (req, res) => {
